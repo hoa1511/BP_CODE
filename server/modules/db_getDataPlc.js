52c828bb032e41a5150cf03358feef6e03c59d38
vovanquynh-max <vovanquynh2000@gmail.com>
2024-11-26 15:15:58 +0700

    test

--- server/modules/db_getDataPlc.js
+++ server/modules/db_getDataPlc.js
@@ -1,46 +1,30 @@
-require('dotenv').config();
-var nodes7 = require('nodes7'); // This is the package name, if the repository is cloned you may need to require 'nodeS7' with uppercase S
-var conns7 = new nodes7;
-const tags = require('./tags_list');
-const tags_list = tags.tags_list();
-const tags_array = tags.tags_array();
 
-function connectPLC() {
-    return new Promise((resolve, reject) => {
-        conns7.initiateConnection({
-            port: 102,
-            host: process.env.IP_PLC || '10.14.84.129',
-            rack: 0, slot: 1, debug: false
-        }, err => {
-            if (typeof (err) !== "undefined") {
-                console.log(err);
-                reject();
+const db_s7_cv = require('./db_s7');
+var values_module;
+
+setTimeout(() => {
+    setInterval(() => {
+        process.send({ event: 'socket_io_message_', data: values_module });
+    }, 500);
+}, 1000);
+
+async function getData() {
+    try {
+        await db_s7_cv.connectPLC();
+        db_s7_cv.readValues((error, values) => {
+            if (error) {
+                console.error("Xảy ra lỗi khi đọc giá trị PLC:", error);
+
             } else {
-                conns7.setTranslationCB(function (tag) { return tags_list[tag]; }); 
-                conns7.addItems(tags_array);
-                resolve();
+                values_module = values;
+                // console.log("Dữ liệu đọc được từ PLC: ", values_module);
             }
         });
-    });
+    } catch (error) {
+        console.error("Xảy ra lỗi đọc plc:", error);
+    }
 };
-
-function readValues(callback) {
-        setInterval(function(){
-            conns7.readAllItems((error, values) =>{
-                if (error) { 
-                    console.log("SOMETHING WENT WRONG READING VALUES PLC"); 
-                    callback(error, null);
-                }else{
-                    callback(null, values);  
-                }
-            });
-        },500);
-}
-
-module.exports = {
-    connectPLC,
-    readValues
-};
+getData();
 
 
-
+ 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-const express = require("express");
 require("dotenv").config();
+const path = require("path");
+
+const express = require("express");
 require('./connection_database');
 const cors = require("cors");
 const app = express();
@@ -22,3 +24,20 @@ app.use('/api/packing', packingRoutes); // Thêm mới
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+
+
+ 
+const { fork } = require("child_process");
+var obj_tag_value = [];
+const tags = require.main.require("./modules/tags_list");
+const threadWriteSql_ = fork(
+  path.join(__dirname, "modules/db_getDataPlc")
+);
+threadWriteSql_.on("message", (message) => {
+  if (message.event === "socket_io_message_") {
+    obj_tag_value = message.data;
+    console.log("Data của plc: ", obj_tag_value);
+  }
+});
+
